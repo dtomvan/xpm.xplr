84e44b386ba7caad2889cb8f2357dc6273327424
Arijit Basu <sayanarijit@users.noreply.github.com>
2022-04-12 20:44:00 +0530

    Branch, revision support and cleanup (#4)
    - Add support for pinning plugins to branch, tag or commits using `rev`
      field.
    - Add support for cleanup and remove plugins.
    - Add setup arguments `plugins`, `auto_install` and `auto_cleanup`,
    - Add a mode to manage plugins manually.
    - Fix/improve cleanup of removable plugins.
    - Some changes.

--- README.md
+++ README.md
@@ -34,17 +34,46 @@ os.execute(
 - Require the module in `~/.config/xplr/init.lua`
 
 ```lua
+
 require("xpm").setup({
+
     -- This works
     'sayanarijit/command-mode.xplr',
+
     -- Or this
     'github:sayanarijit/command-mode.xplr',
+
     -- Or this
     'https://github.com/sayanarijit/command-mode.xplr',
+
     -- You can also use a field
-    name = "sayanarijit/command-mode.xplr",
+    { name = "sayanarijit/command-mode.xplr" },
+})
+
+-- Or
+
+require("xpm").setup({
+  plugins = {
+    'sayanarijit/command-mode.xplr',
+    { name = 'sayanarijit/fzf.xplr' },
+  },
+  auto_install = true,
+  auto_cleanup = true,
 })
 ```
+
+- (optional) Setup key binding for manually managing plugins
+
+```lua
+xplr.config.modes.builtin.default.key_bindings.on_key.x = {
+  help = "xpm",
+  messages = {
+    "PopMode",
+    { SwitchModeCustom = "xpm" },
+  },
+}
+```
+
 WARNING: a current limitation is that any repo not ending in .xplr isn't picked
 up correctly, but by convention the repo's name should end in .xplr
 
@@ -55,21 +84,46 @@ up correctly, but by convention the repo's name should end in .xplr
 - Automatically calls `setup()` if possible
 - Grabs from any git repository
 
-## Config variables
-All available config variables for a plugin are:
+## Setup arguments
+
+```lua
+{
+  -- Default: {}
+  plugins = { "sayanarijit/fzf.xplr" },
+
+  -- Default: true
+  auto_install = true
+
+  -- Default: false
+  auto_cleanup = false
+}
+```
+
+
+## Plugin arguments
+All available arguments for a plugin are:
 ```lua
 -- <in xpm.setup>
 {
     -- Default: Skip if empty
     "<name>",
+
     -- Default: Skip if empty
     name = "<name>",
+
+    -- Branch, commit or tag to pin
+    -- Default: "origin"
+    rev = "<revision>"
+
     -- Default: empty
     after = function() end,
+
     -- Default: empty
     before = function() end,
+
     -- Default: require("<name>").setup()
     setup = function() end,
+
     -- Default: empty
     -- WARNING: if any of the dependencies fail to download, the plugin won't
     -- load to prevent from any damage being done
--- xpm.lua
+++ xpm.lua
@@ -1,68 +1,136 @@
 ---@diagnostic disable
 local xplr = xplr
-_xpm_plugins = {}
 ---@diagnostic enable
 
 local M = {}
 
-local lib = require 'xpm.lib'
-local util = require 'xpm.lib.util'
-
-function M.use_plugin(plugin)
-    local name = plugin.name or plugin[1]
-    if type(plugin) == 'string' then
-        name = plugin
-    end
-    if not name then
-        return true
-    end
-
-    local before = plugin.before
-    if type(before) == 'function' then
-        before()
-    end
-
-    local deps = plugin.deps
-    if type(deps) == 'table' then
-        for _, dep in ipairs(deps) do
-            local status = M.use_plugin(dep)
-            if not status then
-                return false
-            end
-        end
-    end
-
-    local status, _ = lib.add_plugin(name)
-    if status then
-        local config = plugin.setup
-        if type(config) ~= 'function' then
-            config = function()
-                local ok, mod = pcall(require, util.plug_modname(name))
-                if ok then
-                    pcall(mod.setup)
-                end
-            end
-        end
-        config()
-        local after = plugin.after
-        if type(after) == 'function' then
-            after()
-        end
-        local sanitized = util.sanitize_plugin_name(name);
-        if type(plugin) == "string" then
-            plugin = { plugin }
-        end
-        plugin._path = util.plugin_path(sanitized);
-        table.insert(_xpm_plugins, plugin)
-        return true
-    end
-end
+local lib = require("xpm.lib")
 
 function M.setup(args)
-    args = args or {}
-    for _, plugin in ipairs(args) do
-        M.use_plugin(plugin)
-    end
+  args = args or {}
+
+  M.plugins = args.plugins or args
+
+  local auto_install, auto_cleanup = true, false
+
+  if args.auto_install ~= nil then
+    auto_install = args.auto_install
+  end
+
+  if args.auto_cleanup ~= nil then
+    auto_cleanup = args.auto_cleanup
+  end
+
+  if auto_install then
+    M.install_plugins()
+  end
+
+  if auto_cleanup then
+    M.cleanup_plugins()
+  end
+
+  xplr.fn.custom.xpm = {
+    install_plugins = M.install_plugins,
+    cleanup_plugins = M.cleanup_plugins,
+    update_plugins = M.update_plugins,
+    remove_plugins = M.remove_plugins,
+
+    render_plugins = function(_)
+      local ui = { " " }
+      for name, plugin in pairs(lib._xpm_plugins) do
+        table.insert(ui, string.format("%s (%s)", name, plugin.rev or "origin"))
+      end
+      return ui
+    end,
+  }
+
+  xplr.config.modes.custom.xpm = {
+    name = "xpm",
+
+    layout = {
+      Horizontal = {
+        config = {
+          constraints = {
+            { Percentage = 50 },
+            { Percentage = 50 },
+          },
+        },
+        splits = {
+          {
+            CustomContent = {
+              title = "plugins",
+              body = {
+                DynamicList = { render = "custom.xpm.render_plugins" },
+              },
+            },
+          },
+          "HelpMenu",
+        },
+      },
+    },
+
+    key_bindings = {
+      on_key = {
+        i = {
+          help = "install plugins",
+          messages = {
+            { CallLua = "custom.xpm.install_plugins" },
+          },
+        },
+        c = {
+          help = "cleanup plugins",
+          messages = {
+            { CallLua = "custom.xpm.cleanup_plugins" },
+          },
+        },
+        u = {
+          help = "update plugins",
+          messages = {
+            { CallLua = "custom.xpm.update_plugins" },
+          },
+        },
+        r = {
+          help = "remove plugins",
+          messages = {
+            { CallLua = "custom.xpm.remove_plugins" },
+          },
+        },
+        esc = {
+          messages = {
+            "PopMode",
+          },
+        },
+        ["ctrl-c"] = {
+          messages = {
+            "Terminate",
+          },
+        },
+      },
+    },
+  }
+end
+
+M.use_plugin = lib.use_plugin
+
+function M.install_plugins(_)
+  for _, plugin in ipairs(M.plugins) do
+    M.use_plugin(plugin)
+  end
+end
+
+function M.update_plugins(_)
+  lib.update_plugins()
+end
+
+function M.cleanup_plugins(_)
+  local removable = lib.removable_plugins()
+  for _, plugin in ipairs(removable) do
+    lib.remove_plugin(plugin)
+  end
+end
+
+function M.remove_plugins(_)
+  lib.remove_plugins()
 end
 
 return M
